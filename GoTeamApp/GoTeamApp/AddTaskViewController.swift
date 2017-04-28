18adb883dde816810ca709625c105610cb0177fc
abhandary <akshay.bhandary@gmail.com>
2017-04-28 00:06:18 -0700

    date button works

--- GoTeamApp/GoTeamApp/AddTaskViewController.swift
+++ GoTeamApp/GoTeamApp/AddTaskViewController.swift
@@ -11,11 +11,14 @@ import UIKit
 
 enum TableState {
     case none
+    case date
+    case dueDate
     case priority
 }
 
 class AddTaskViewController: UIViewController {
 
+    static let dateFormatter = DateFormatter()
     
     let kAddTaskCell = "AddTaskCell"
     
@@ -38,6 +41,10 @@ class AddTaskViewController: UIViewController {
     var tableState : TableState = .none
     
     let priorityArray = ["1 - High", "2 - Medium", "3 - Low", "None"]
+    var dateArray = ["Today", "Tomorrow", "", "", "", "1 week", "No due date"]
+    
+    var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,10 +66,28 @@ class AddTaskViewController: UIViewController {
         
         // setup button
         setupPriorityButton()
+        
+        // setup due date
+        setupDate()
+    }
+    
+    func setupDate() {
+        let today = Date()
+        let dayAfter = Calendar.current.date(byAdding: .day, value: 2, to: today)
+        AddTaskViewController.dateFormatter.dateFormat = "EEEE"
+        dateArray[2] = AddTaskViewController.dateFormatter.string(from: dayAfter!)
+        let dayAfterThat = Calendar.current.date(byAdding: .day, value: 3, to: today)
+        dateArray[3] = AddTaskViewController.dateFormatter.string(from: dayAfterThat!)
+        let nextDayAfterDayAfter = Calendar.current.date(byAdding: .day, value: 4, to: today)
+        dateArray[4] = AddTaskViewController.dateFormatter.string(from: nextDayAfterDayAfter!)
+        
+        dateButton.isHighlighted = false
+        let dateButtonTapGR = UITapGestureRecognizer(target: self, action: #selector(dateButtonTapped))
+        dateButton.addGestureRecognizer(dateButtonTapGR)
     }
 
     func setupPriorityButton() {
-         priorityButton.isHighlighted = false
+        priorityButton.isHighlighted = false
         let priorityTapGR = UITapGestureRecognizer(target: self, action: #selector(priorityButtonTapped))
         priorityButton.addGestureRecognizer(priorityTapGR)
     }
@@ -76,28 +101,49 @@ class AddTaskViewController: UIViewController {
         
     }
 
+    func dateButtonTapped(sender : UITapGestureRecognizer) {
+        handleButtonTapped(state: .date, char : "^")
+    }
 
     
     func priorityButtonTapped(sender : UITapGestureRecognizer) {
+        handleButtonTapped(state: .priority, char : "!")
+    }
+    
+    func handleButtonTapped(state: TableState, char : String) {
         let text = textView.text
         let textArray = Array(text!.characters)
-
+        
         if textArray[textArray.count - 1] != " " {
             textView.text = textView.text + " "
         }
-        textView.text = textView.text + "!"
+        textView.text = textView.text + char
         tableView.isHidden = false
         maskView.isHidden = true
-        tableState = .priority
+        tableState = state
         tableView.reloadData()
     }
 }
 
 extension AddTaskViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        switch tableState {
+        case .priority:
+            return 1
+        case .date:
+            return 1
+        default:
+            return 0
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch tableState {
         case .priority:
             return priorityArray.count
+        case .date:
+            return dateArray.count
         default:
             return 0
         }
@@ -108,6 +154,10 @@ extension AddTaskViewController : UITableViewDelegate, UITableViewDataSource {
         case .priority:
             let cell = self.tableView.dequeueReusableCell(withIdentifier: kAddTaskCell) as! AddTaskCell
             cell.textLabel?.text = priorityArray[indexPath.row]
+            return cell
+        case .date:
+            let cell = self.tableView.dequeueReusableCell(withIdentifier: kAddTaskCell) as! AddTaskCell
+            cell.textLabel?.text = dateArray[indexPath.row]
             return cell
         default:
             return UITableViewCell()
@@ -125,6 +175,15 @@ extension AddTaskViewController : UITableViewDelegate, UITableViewDataSource {
                 textView.text = textView.text + String(indexPath.row + 1)
                 priorityButton.isUserInteractionEnabled = false
                 priorityButton.isHighlighted = true
+            }
+        case .date:
+            if indexPath.row == dateArray.count - 1  {
+                let chars = Array(textView.text.characters)
+                textView.text = String(chars[0..<chars.count - 2])
+            } else {
+                textView.text = textView.text + dateArray[indexPath.row]
+                dateButton.isUserInteractionEnabled = false
+                dateButton.isHighlighted = true
             }
         default: break
         }
@@ -156,6 +215,18 @@ extension AddTaskViewController : UITextViewDelegate {
         } else {
             priorityButton.isHighlighted = true
             priorityButton.isUserInteractionEnabled = false
+        }
+        
+        
+        dateButton.isHighlighted = false
+        dateButton.isUserInteractionEnabled = true
+        for date in dateArray {
+            let testString = "^" + date
+            if textView.text.contains(testString) {
+                dateButton.isHighlighted = true
+                dateButton.isUserInteractionEnabled = false
+                break
+            }
         }
     }
     
